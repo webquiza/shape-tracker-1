465ae64c5037482e1656d07ca58827c540de9296
Carlos Urquiza <webquiza@gmail.com>
2021-07-18 10:46:15 -0700

    add additional project files
    Co-authored-by: Amerik Velasquez <amerik.velasquez22@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('#triangle-checker-form').submit(function(event) {
+    event.preventDefault();
+    const length1 = $('#length1').val();
+    const length2 = $('#length2').val();
+    const length3 = $('#length3').val();
+    const triangle = new Triangle(length1, length2, length3)
+    const response = triangle.checkType();
+    $('#response').append("<p>" + response + "</p>");
+  });
+});
