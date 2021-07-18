01d4f8e68e3c2ed0e2d8398d366007b1e5f4ba9a
Carlos Urquiza <webquiza@gmail.com>
2021-07-18 14:53:54 -0700

    update webpack project
    Co-authored-by: Amerik Velasquez <amerik.velasquez22@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+import Triangle from './triangle.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
 $(document).ready(function() {
   $('#triangle-checker-form').submit(function(event) {
     event.preventDefault();
     const length1 = $('#length1').val();
     const length2 = $('#length2').val();
     const length3 = $('#length3').val();
-    const triangle = new Triangle(length1, length2, length3)
+    const triangle = new Triangle(length1, length2, length3);
     const response = triangle.checkType();
     $('#response').append("<p>" + response + "</p>");
   });
--- src/triangle.js
+++ src/triangle.js
@@ -1,4 +1,4 @@
-function Triangle(side1, side2, side3) {
+export default function Triangle(side1, side2, side3) {
   this.side1 = side1;
   this.side2 = side2;
   this.side3 = side3;
@@ -6,4 +6,5 @@ function Triangle(side1, side2, side3) {
 
 Triangle.prototype.checkType = function() {
   return "I can't answer that yet!";
-}
+};
+
