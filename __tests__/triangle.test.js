0731c266ba2cdf253584fcefdc7838df8551f428
Carlos Urquiza <webquiza@gmail.com>
2021-07-19 21:28:43 -0700

    add additional passing test for Triangle constructor

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -18,6 +18,16 @@ describe('Triangle', () => {
     const scalTriangle = new Triangle(4,5,7)
     expect(scalTriangle.checkType()).toEqual("scalene triangle");
   });
-  
+
+  test('should correctly determine whether three lengths make an isosceles triangle', () => {
+    const isocTriangle = new Triangle(5,5,7)
+    expect(isocTriangle.checkType()).toEqual("isosceles triangle");
+  });
+
+  test('should correctly determine whether three lengths make an equilateral triangle', () => {
+    const equiTriangle = new Triangle(5,5,5)
+    expect(equiTriangle.checkType()).toEqual("equilateral triangle");
+  });
+
 });
 
