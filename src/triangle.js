afcbbf56e980466f94ee9a57e7da6b0ec8006dca
Carlos Urquiza <webquiza@gmail.com>
2021-07-20 19:17:44 -0700

    update project
    Co-authored-by: Amerik Velasquez <amerik.velasquez22@gmail.com>

--- src/triangle.js
+++ src/triangle.js
@@ -9,8 +9,10 @@ Triangle.prototype.checkType = function() {
     return "not a triangle";
   } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
     return "scalene triangle";
-  }  else if ((this.side1 === this.side2) || ((this.side1 === this.side3)) || ((this.side2 === this.side3))) {
-      return "isosceles triangle";
-    }
+  } else if ((this.side1 === this.side2) && (this.side1 === this.side3)) {
+      return "equilateral triangle";
+  } else {
+    return "isosceles triangle";
+  }
 };
 
