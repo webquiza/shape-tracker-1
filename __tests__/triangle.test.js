9e75d8d7337c9c9b3cf4cf47763990aa21bcd8ad
Carlos Urquiza <webquiza@gmail.com>
2021-07-20 19:16:58 -0700

    add fifth passing test
    Co-authored-by: Amerik Velasquez <amerik.velasquez22@gmail.com>

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -22,4 +22,9 @@ describe('Triangle', () => {
     const isocTriangle = new Triangle(5,5,7)
     expect(isocTriangle.checkType()).toEqual("isosceles triangle");
   });
+
+  test('should correctly determine whether three lengths make an equilateral triangle', () => {
+    const equiTriangle = new Triangle(5,5,5)
+    expect(equiTriangle.checkType()).toEqual("equilateral triangle");
+  });
 });
