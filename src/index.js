70707fad9b5830fa18bf3441df9eedd260bf2bc7
Carlos Urquiza <webquiza@gmail.com>
2021-07-25 11:33:42 -0700

    resolve './triangle.js' path issue

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Triangle from './triangle.js';
+import Triangle from './js/triangle.js';
 import Rectangle from './js/rectangle.js';
 
 $(document).ready(function() {
